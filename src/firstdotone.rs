3f4ed1e389397773a1b258e7a35927eec85577cb
ydrea <ydrea.wrld@gmail.com>
2024-09-18 17:40:31 +0200

    drop

--- src/firstdotone.rs
+++ src/firstdotone.rs
@@ -1,43 +1,63 @@
 use std::mem;
-
+///List = Link::More(Box<Node>) =+ Link::More(Box<Node>) =+ Link::Empty
+//init type List, first pointer...
 pub struct List {
-    head: Link,
+ pub a: Link,
 }
-
-enum Link {
+///...to either nothing, or something more...
+pub enum Link {
     Empty,
     More(Box<Node>),
 }
-
-struct Node {
+///...shaped like a Node, on the heap (Box).
+pub struct Node {
     elem: i32,
     next: Link
 }
-
+///create an empty List
 impl List {
-    pub fn new(self: Self) -> Self { 
-        return List { head: Link::Empty };   
+    pub fn new(&self) -> Self { 
+        return List { a: Link::Empty };   
     }
-
-    pub fn push(self: &mut Self, elem: i32) {
+///push a node to List
+    pub fn push(&mut self, elem: i32) {
+ //make a new_node to be Box::new(elem, next: self.a)
         let new_node = Box::new(Node {
             elem: elem,
-            next: mem::replace(&mut self.head, Link::Empty),
+            //temporarily replacing self.a with an empty Link 
+            next: mem::replace(&mut self.a, Link::Empty),
         });
-        self.head = Link::More(new_node);
+        //< returning self.a to be the new_node
+        self.a = Link::More(new_node);
     }
-
-    pub fn pop(self: &mut Self) -> Option<i32> {
+///pop a Node off the List
+///Option::Some<T> || None, to check for empty List
+    pub fn pop(&mut self) -> Option<i32> {
+        //init result to be returned
         let result; 
-        match mem::replace(&mut self.head, Link::Empty) {
+        //mach on Option
+        //+ replace, temporarily replacing self.a with an empty Link 
+        match mem::replace(&mut self.a, Link::Empty) {
             Link::Empty => {
                 result = None;
-            }
+            },
             Link::More(node) => {
-                result= Some(node.elem);
-                self.head = node.next;
-            }
+                result = Some(node.elem);
+                self.a = node.next;
+            },
         };
         result
+    }
+}
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut drop_link = mem::replace(&mut self.a, Link::Empty);
+        // while let == do this thing until this pattern stops matching
+            // boxed_node goes out of scope and gets dropped here...
+        while let Link::More(mut boxed_node) = drop_link {
+
+//  ...while Node::next field = Link::Empty.
+            drop_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        } //so no unbounded recursion occurs
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub mod first;
+// pub mod first;
 pub mod firstdotone;
 
 /// Adds two
@@ -7,12 +7,34 @@ pub fn add(left: u64, right: u64) -> u64 {
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
-
+    use crate::firstdotone::{List, Link};
+    
     #[test]
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+
+    #[test]
+    fn testis (){
+        let mut list = List::new(&firstdotone::List{a: Link::Empty});
+        assert_eq!(list.pop(), None);
+
+        list.push(1);   
+        list.push(2);   
+        // list.push();   
+
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+        
+    }
+    
+    
+
+
+
 }
